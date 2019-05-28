01056e5b64ea1920f33e0dd35b1ae20b30c5e0b1
Ricardas <Ricardaskysla@gmail.com>
2019-05-28 11:48:55 +0300

    Home work Nr. 13
    Testing Js

---
+++ Namu darbas Nr. 13/js.js
@@ -0,0 +1,49 @@
+// // function homework() {
+// //
+// //
+// //     var length = document.getElementById("length").value;
+// //     var breadth = document.getElementById("breadth").value;
+// //
+// //
+// //     var perimeter = 2 * (length + breadth);
+// //     var area = length * breadth;
+// //     var diagonal = Math.sqrt((length * length) + (breadth * breadth));
+// //
+// //
+// //     console.log(perimeter, area, diagonal);
+// //
+// //     document.getElementById("homework").innerHTML = perimeter + " Plotas: " + area + " Istrižainė: " + diagonal;
+// //
+// //
+// // }
+//
+// function perimeter() {
+//     var length = document.getElementById("length").value;
+//     var breadth = document.getElementById("breadth").value;
+//     var perimeter = 2 * (length + breadth);
+//     var x = perimeter;
+//     var m = x.toFixed(2);
+//
+//
+//     document.getElementById("perimeter").innerHTML = m ;
+// }
+//
+// function area() {
+//     var length = document.getElementById("length").value;
+//     var breadth = document.getElementById("breadth").value;
+//     var area = length * breadth;
+//     var y = area;
+//     var n = y.toFixed(2);
+//
+//     document.getElementById("area").innerHTML = n ;
+// }
+//
+// function diagonal() {
+//     var length = document.getElementById("length").value;
+//     var breadth = document.getElementById("breadth").value;
+//     var diagonal = Math.sqrt((length * length) + (breadth * breadth));
+//     var z = diagonal;
+//     var l = z.toFixed(2);
+//
+//     document.getElementById("diagonal").innerHTML = l ;
+// }
