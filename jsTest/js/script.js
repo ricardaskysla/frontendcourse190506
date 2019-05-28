96e285f598ab467f5910fb565e3cfe28fd9938c2
Ricardas <Ricardaskysla@gmail.com>
2019-05-28 11:47:28 +0300

    Testing Bootstrap

---
+++ jsTest/js/script.js
@@ -0,0 +1,22 @@
+
+// function changeNameToSurname () {
+//
+//
+// document.getElementById("name").innerHTML ="<span style='color: red;'>Restoranas</span>";
+// console.log(2+2);
+// }
+//
+// document.getElementById("banduymas").innerHTML ="<span style='color: red;'>Belekas2</span>";
+
+
+function naujas () {
+    var name = "Vardas"; //Pasizymeti ar tekstas ar skaicius nurodant tipa
+    var surname = "Pavarde";
+    var age = 50;
+    var ageAfterMYears = age + 5;
+    console.log(ageAfterMYears);
+
+    document.getElementById("naujas").innerHTML =name + " " + surname;
+
+
+}
